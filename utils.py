e7af5d9f28e3ee04efd50ee7c3434ae0b2d3465b
Adam Oswald <AdamOswald1@users.noreply.huggingface.co>
2022-11-07 16:42:57 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,12 @@
+from io import BytesIO
+from PIL import Image
+
+
+def convert_bytes_to_pil(bytes_string: bytes) -> Image:
+    buffer = BytesIO(bytes_string)
+    buffer.seek(0)
+    return Image.open(buffer)
+
 def is_google_colab():
     try:
           import google.colab
